1c343138fc80c04cc596b43de2d2a01aafa8d563
Arnab Roy <trifiasco@gmail.com>
2020-04-11 14:12:35 +0600

    Updates

---
+++ precache-manifest.409bff422506f2a3f9501039d2c6bfa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10b9458ee26be0993eefe1e02763bcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7338e7d6a2c9ebbf18c7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ce673958ba21e54bd3d",
+    "url": "/static/js/2.1eeee909.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1eeee909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7338e7d6a2c9ebbf18c7",
+    "url": "/static/js/main.36cc4b5a.chunk.js"
+  },
+  {
+    "revision": "f7f33f356c4795300a7f",
+    "url": "/static/js/runtime-main.b6a74aed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
