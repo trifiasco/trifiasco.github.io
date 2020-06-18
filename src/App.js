a11ea86d27be862236f8332e1e8f8f3bd2f5ec69
Arnab Roy <trifiasco@gmail.com>
2020-06-19 00:35:11 +0600

    Add particle background and restructure

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import Sidebar from './components/sidebar';
-import Home from './components/home';
-import About from './components/about';
-import Experience from './components/experience';
-import Education from './components/education';
-import Skills from './components/skills';
-import Projects from './components/projects';
-import Contact from './components/contact';
-import logo from './logo.svg';
 import './App.css';
-import backgroundPic from './images/background1.png'
+import ParticlesBg from 'particles-bg';
+import Main from './components/main';
 
 function App() {
   return (
-    <div id="template-page">
-      <div id="container-wrap">
-        <Sidebar></Sidebar>
-        <div id="template-main">
-          <Home></Home>
-          <About></About>
-          <Experience></Experience>
-          <Skills></Skills>
-          <Education></Education>
-          {/* <Projects></Projects> */}
-          <Contact></Contact>
-        </div>
-      </div>
+    <div>
+      <div id="particle" style={{zIndex: 2}}><ParticlesBg type="cobweb" bg={true}/></div>
+      <Main />
     </div>
   );
 }
