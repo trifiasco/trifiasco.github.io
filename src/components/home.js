9b1b11b0627f3a82422550bfeda69773f03ef858
Arnab Roy <trifiasco@gmail.com>
2020-06-18 23:42:06 +0600

    Clean up home and fix slider

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import FullPageDialog from './utils/fullPageDialog';
 
 const Home = () => {
-    const resumePath = `${process.env.PUBLIC_URL}/static files/Arnab_Roy_Resume.pdf`;
-
     return (
         <div>
         <section id="template-hero" className="js-fullheight" data-section="home">
@@ -18,15 +16,10 @@ const Home = () => {
                         <div className="desc">
                           <h1>Hi! <br />I'm Arnab</h1>
                           <FullPageDialog />
-                          {/* <p><a className="btn btn-primary btn-learn" onClick={}  >View CV<i className="icon-download4" /></a></p> */}
                         </div>
                       </div>
                     </div>
                   </div>
-                  {/* href="https://drive.google.com/open?id=1b1daoJn6eziuvpmKOcSCvQZn1qI2AXOo" target="_blank" rel="noopener noreferrer" */}
-                  {/* <div id='viewer' style={{width: '100%', height:'100%'}}>
-                    <object width="100%" height="400" data={resumePath} type="application/pdf">   </object>
-                  </div> */}
                 </div>
               </li>
               <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
