1c60f3d0323ece102e3628dcd907c089174fc49b
Arnab Roy <trifiasco@gmail.com>
2020-08-25 14:05:51 +0600

    minor refactor

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Sidebar from '../components/sidebar';
-import Home from '../components/home';
-import About from '../components/about';
-import Experience from '../components/experience';
-import Education from '../components/education';
-import Skills from '../components/skills';
-import Achievements from '../components/achievements';
-import Projects from '../components/projects';
-import Contact from '../components/contact';
-import logo from '../logo.svg';
-import '../App.css';
-import backgroundPic from '../images/background1.png';
+import React from "react";
+import Sidebar from "../components/sidebar";
+import Home from "../components/home";
+import About from "../components/about";
+import Experience from "../components/experience";
+import Education from "../components/education";
+import Skills from "../components/skills";
+import Achievements from "../components/achievements";
+import Projects from "../components/projects";
+import Contact from "../components/contact";
+import logo from "../logo.svg";
+import "../App.css";
+import backgroundPic from "../images/background1.png";
 
 function Main() {
   return (
