d7a2898d604d0d073f35d7a55bf4fbef4174db33
Arnab Roy <trifiasco@gmail.com>
2020-08-25 14:02:52 +0600

    update resume view

--- src/components/home.js
+++ src/components/home.js
@@ -1,62 +1,98 @@
-import React from 'react';
-import FullPageDialog from './utils/fullPageDialog';
+import React from "react";
+import Resume from "../info/trifiasco-resume.pdf";
 
 const Home = () => {
-    return (
-        <div>
-        <section id="template-hero" className="js-fullheight" data-section="home">
-          <div className="flexslider js-fullheight">
-            <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner js-fullheight">
-                        <div className="desc">
-                          <h1>Hi! <br />I'm Arnab</h1>
-                          <FullPageDialog />
-                        </div>
+  return (
+    <div>
+      <section id="template-hero" className="js-fullheight" data-section="home">
+        <div className="flexslider js-fullheight">
+          <ul className="slides">
+            <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner js-fullheight">
+                      <div className="desc">
+                        <h1>
+                          Hi! <br />
+                          I'm Arnab
+                        </h1>
+                        <p>
+                          <a
+                            className="btn btn-primary btn-learn"
+                            href={Resume}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            View Resume <i className="icon-document" />
+                          </a>
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/trifiasco" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
-                        </div>
+              </div>
+            </li>
+            <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner">
+                      <div className="desc">
+                        <h1>
+                          I love building
+                          <br /> THINGS !!
+                        </h1>
+                        <p>
+                          <a
+                            className="btn btn-primary btn-learn"
+                            href="https://github.com/trifiasco"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            View Projects <i className="icon-briefcase3" />
+                          </a>
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://trifiasco.wordpress.com/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
-                        </div>
+              </div>
+            </li>
+            <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner">
+                      <div className="desc">
+                        <h1>
+                          I often <br />
+                          Write ...{" "}
+                        </h1>
+                        <p>
+                          <a
+                            className="btn btn-primary btn-learn"
+                            href="https://trifiasco.wordpress.com/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            View Blog <i className="icon-book" />
+                          </a>
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </li>
-            </ul>
-          </div>
-        </section>
-      </div>
-    );
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
